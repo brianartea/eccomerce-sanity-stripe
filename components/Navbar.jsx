e7be440c184d2bc886eb92b447b64b09b01d0c66
brianartea <bri@expertdesign.cc>
2022-04-24 16:37:53 -0400

    navbar styling and object-fit: contain; on banner imgs

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -11,7 +11,7 @@ const Navbar = () => {
   return (
     <div className="navbar-container">
       <p className="logo">
-        <Link href="/">Zilo Headphones</Link>
+        <Link href="/">Zilo Gadgets</Link>
       </p>
       <button
         type="submit"
